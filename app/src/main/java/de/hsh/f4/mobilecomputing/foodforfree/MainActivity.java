8e9255560a50869e976a8801f12d6fb9e8a283bf
laura97ng <laura.nguyen1236@yahoo.de>
2020-11-02 20:11:27 +0100

    Navigation Drawer Version 1

--- app/src/main/java/de/hsh/f4/mobilecomputing/foodforfree/MainActivity.java
+++ app/src/main/java/de/hsh/f4/mobilecomputing/foodforfree/MainActivity.java
@@ -1,5 +1,6 @@
 package de.hsh.f4.mobilecomputing.foodforfree;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,15 +11,16 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(), Login.class));
-        finish();
-    }
+
+    //public void logout(View view) {
+    //    FirebaseAuth.getInstance().signOut();
+    //    startActivity(new Intent(getApplicationContext(), Login.class));
+    //    finish();
+    //}
 }
