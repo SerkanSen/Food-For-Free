30384143c7c4c3346d42df93d64c77a24f77814f
laura97ng <laura.nguyen1236@yahoo.de>
2020-11-14 11:01:08 +0100

    Profilbild hochladen und in Firebase Storage speichern, im Profil angezeigt

--- app/src/main/java/de/hsh/f4/mobilecomputing/foodforfree/Profile.java
+++ app/src/main/java/de/hsh/f4/mobilecomputing/foodforfree/Profile.java
@@ -1,5 +1,6 @@
 package de.hsh.f4.mobilecomputing.foodforfree;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,24 +9,39 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity {
     //Initialize variable
     DrawerLayout drawerLayout;
     TextView name, email, adresse, tName, tEmail, tAdresse;
+    ImageView profilePhoto;
+    Button uploadProfilePhotoBtn;
+    StorageReference storageReference;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +56,20 @@ public class Profile extends AppCompatActivity {
         name = findViewById(R.id.profileName);
         email = findViewById(R.id.profileMail);
 
+        profilePhoto = findViewById(R.id.profilePhoto);
+        uploadProfilePhotoBtn = findViewById(R.id.uploadProfilePhotoBtn);
+
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profilePhoto);
+            }
+        });
 
         userId = fAuth.getCurrentUser().getUid();
 
@@ -54,8 +82,52 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        uploadProfilePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                profilePhoto.setImageURI(imageUri);
+
+                uploadImageToFirebase(imageUri);
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        //upload image to firebase storage
+        StorageReference fileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //Toast.makeText(Profile.this, "Profilbild erfolgreich hochgeladen.", Toast.LENGTH_SHORT).show();
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profilePhoto);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Profile.this, "Fehlgeschlagen.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     public void ClickMenu(View view) {
         //Open Drawer
         MainActivity.openDrawer(drawerLayout);
